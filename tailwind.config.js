cfee134719b1faf581d44e7a5ac357ccfa90812d
ttowntom <git.running054@passinbox.com>
2024-04-03 15:39:11 +0200

    set up page layout and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./*html"],
+	content: ["./**/*.html"],
 	darkMode: "class",
 	theme: {
 		extend: {
@@ -10,9 +10,6 @@ module.exports = {
 				greenDark: "#274431",
 				desertTan: "#f3f3f3",
 				almostWhite: "#f5f8f7",
-				"wGray-100": "#a4a4a4",
-				"wGray-200": "#5a5f6b",
-				"wGray-300": "#444652",
 			},
 			fontFamily: {
 				sans: ["Raleway", "sans-serif"],
