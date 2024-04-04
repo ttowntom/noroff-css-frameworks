549ebb8b0a45f36e7cdc2737563edbe67e7e2f17
ttowntom <git.running054@passinbox.com>
2024-04-04 08:41:08 +0200

    profile page layout and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
 				darkBlue: "hsl(217, 28%, 15%)",
 				greenPrimary: "#58ae9c",
 				greenHover: "#4a9181",
+				greenHoverLight: "#83ecd5",
 				greenDark: "#274431",
 				desertTan: "#f3f3f3",
 				almostWhite: "#f5f8f7",
